20243c79ea260b69ab3daa3970a63ea3f269495f
Alexandr Gedz <mygedz@gmail.com>
2025-03-11 22:25:39 +0300

    Fix logging issues

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -10,10 +10,29 @@ import (
 
 const logDir = "logs"
 
+var (
+	logFile    *os.File
+	logger     *log.Logger
+	logChannel chan string
+)
+
 func init() {
 	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
 		log.Fatalf("[ERROR] Папка для логов не создана: %v", err)
 	}
+
+	logFilePath := getLogFilePath("log")
+	var err error
+	logFile, err = os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("[ERROR] Не удалось открыть файл лога: %v", err)
+	}
+
+	logger = log.New(logFile, "", log.LstdFlags)
+
+	logChannel = make(chan string, 100)
+
+	go processLogs()
 }
 
 func getLogFilePath(logsType string) string {
@@ -23,20 +42,24 @@ func getLogFilePath(logsType string) string {
 	return filepath.Join(logDir, fileName)
 }
 
-func LogMessage(logsType, message string) error {
-	logFilePath := getLogFilePath(logsType)
-
-	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return fmt.Errorf("[ERROR] Не удалось открыть файл лога: %w", err)
+func processLogs() {
+	for msg := range logChannel {
+		logger.Println(msg)
 	}
-	defer file.Close()
+}
 
+func LogMessage(logsType, message string) {
 	timestamp := time.Now().Format("02.01.2006 15:04:05")
 	formattedMessage := fmt.Sprintf("[%s] %s", timestamp, message)
 
-	logger := log.New(file, "", 0)
-	logger.Println(formattedMessage)
+	select {
+	case logChannel <- formattedMessage:
+	default:
+		fmt.Println("[ERROR] Логгер перегружен, сообщение пропущено:", formattedMessage)
+	}
+}
 
-	return nil
+func CloseLogger() {
+	close(logChannel)
+	logFile.Close()
 }
